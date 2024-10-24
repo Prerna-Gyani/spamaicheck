cd33a69e1e063ffedf6e6ea93595062743dc9f6f
Prerna Gyani <142766837+Prerna-Gyani@users.noreply.github.com>
2024-10-24 11:35:02 +0530

    Delete spamsentrypixalai/spamsentrypixalaiprerna/spamsentrypixalaiprerna directory

--- spamsentrypixalai/spamsentrypixalaiprerna/spamsentrypixalaiprerna/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.template import loader
-from django.shortcuts import render
-import requests
-import os
-
-def home(request):
-    return render(request, 'spamsentrypixalaiprerna/mainpg.html')
