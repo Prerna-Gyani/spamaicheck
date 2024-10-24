681dc285c5c05a493c5c4adf200212b1653ce562
Prerna Gyani <142766837+Prerna-Gyani@users.noreply.github.com>
2024-10-24 12:45:56 +0530

    Delete members/views.py

--- members/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.template import loader
-from django.shortcuts import render
-import requests
-
-def home(request):
-    return render(request, 'mainpg.html')
