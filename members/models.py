d945ab818d7fce3b90cba7a4972f12a1cd3d665b
Prerna Gyani <142766837+Prerna-Gyani@users.noreply.github.com>
2024-11-25 04:48:01 +0530

    Delete old members directory

--- members/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class SpamWebsite(models.Model):
-    keyword = models.CharField(max_length=255)
-    spam_type = models.CharField(max_length=50)  # Make sure this matches your choices
-    # Add other fields if necessary
-
-    def __str__(self):
-        return self.keyword
--- members/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('detect/', views.detect_spam, name='detect_spam'),
-    path('spam-listing/', views.spam_website_listing, name='spam_listing'),  
-]
